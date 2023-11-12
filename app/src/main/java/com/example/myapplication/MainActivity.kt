173b45aaf85c4577f603a83a0d7973531dcb1395
MuktharAmiyan <amiyanmukthar@gmail.com>
2023-11-12 15:59:07 +0530

    button done

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,15 +2,22 @@
 package com.example.myapplication
 
 import android.os.Bundle
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,24 +29,35 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(color = Color.Black.copy(0.8f)),
+                    modifier = Modifier.fillMaxSize(),
                     contentAlignment = Alignment.Center
-                ){
-                    MyCustomCard(
-                        modifier = Modifier.fillMaxWidth(0.8f),
-                        image = R.drawable.image,
-                        title = "Basketball Shoes for the Win!",
-                        text = "Find your game-changing pair of basketball shoes. Score big on the court with style and performance. Explore our top picks now!",
-                        publisher = Publisher(name = "Mukthar", job = "Software Engineer" , image = R.drawable.person)
-                    )
+                ) {
+                    Button(
+                        modifier = Modifier
+                            .height(50.dp)
+                            .width(140.dp),
+                        shape = RoundedCornerShape(
+                            topStart = 10.dp,
+                            bottomEnd = 10.dp
+                        ),
+                        enabled = true,
+                        elevation = ButtonDefaults.buttonElevation(
+                            defaultElevation = 20.dp
+                        ),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Red,
+                            contentColor = Color.Black,
+                            disabledContainerColor = Color.LightGray,
+                            disabledContentColor = Color.Gray
+                        ),
+                        onClick = { }
+                    ) {
+                        Text(text = "Click Me")
+                    }
                 }
             }
         }
     }
-
-
 }
 
 
